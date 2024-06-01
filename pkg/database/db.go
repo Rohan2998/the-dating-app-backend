f63448c33383b95fe574dfdf18aceb8ba3d773e0
Rohan2998 <ronsnow98@gmail.com>
2024-06-01 09:42:03 +0530

    [chores] added config and sql connection functionality

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"database/sql"
+
+	"github.com/Rohan2998/the-dating-app-backend/config"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// create a connection to database
+func Connect(cfg *config.Config) (*sql.DB, error) {
+	dsn := cfg.DBUser + ":" + cfg.DBPassword + "@tcp(" + cfg.DBHost + ":" + cfg.DBPort + ")/" + cfg.DBName
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
