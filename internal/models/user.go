9a8df8e35c28dd526924c69262f50d11e8b3cfc6
Rohan2998 <ronsnow98@gmail.com>
2024-06-07 15:02:56 +0530

    [fix]

--- internal/models/user.go
+++ internal/models/user.go
@@ -9,19 +9,21 @@ import (
 
 // User represents a user in the system
 type User struct {
-	ID        uuid.UUID  `json:"id"`
-	Name      string     `json:"name"`
-	Username  string     `json:"username"`
-	Email     string     `json:"email"`
-	Password  *string    `json:"-"`
-	DOB       *time.Time `json:"dob"`
-	CreatedAt *time.Time `json:"created_at"`
-	UpdatedAt *time.Time `json:"updated_at"`
+	ID            uuid.UUID  `json:"id" db:"id"`
+	Name          string     `json:"name" db:"name"`
+	Username      string     `json:"username" db:"username"`
+	Email         string     `json:"email" db:"email"`
+	Password      *string    `json:"-" db:"password"`
+	CountryAbbr   *string    `json:"countryAbbr" db:"country_abbr"`
+	CountryCode   *string    `json:"countryCode" db:"country_code"`
+	ContactNumber *string    `json:"contactNumber" db:"contact_number"`
+	DOB           *time.Time `json:"dob" db:"dob"`
+	CreatedAt     *time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt     *time.Time `json:"updatedAt" db:"updated_at"`
 }
 
 // create a new user
 func NewUser(fields map[string]interface{}) (*User, error) {
-
 	// get default timestamp
 	now := time.Now()
 
@@ -40,6 +42,21 @@ func NewUser(fields map[string]interface{}) (*User, error) {
 		newUser.Password = &password
 	}
 
+	// handle country abbreviation
+	if countryAbbr, ok := fields["countryAbbr"].(string); ok {
+		newUser.CountryAbbr = &countryAbbr
+	}
+
+	// handle country code
+	if countryCode, ok := fields["countryCode"].(string); ok {
+		newUser.CountryCode = &countryCode
+	}
+
+	// handle country code
+	if contactNumber, ok := fields["contactNumber"].(string); ok {
+		newUser.ContactNumber = &contactNumber
+	}
+
 	// handle dob field
 	if bd, ok := fields["dob"].(time.Time); ok {
 		newUser.DOB = &bd
@@ -48,5 +65,4 @@ func NewUser(fields map[string]interface{}) (*User, error) {
 	}
 
 	return newUser, nil
-
 }
